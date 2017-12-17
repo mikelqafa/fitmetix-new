30b34bb9eaeb77099f188407c359338b9545c87f
Ankur Agarwal <ankur@novatree.com>
2017-12-17 07:51:41 +0530

    General settings page changed

--- public/themes/default/views/users/settings/general.blade.php
+++ public/themes/default/views/users/settings/general.blade.php
@@ -2,11 +2,12 @@
 <!-- <div class="main-content"> -->
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
+			{{--<div class="col-md-4">
 				<div class="post-filters">
 					{!! Theme::partial('usermenu-settings') !!}
 				</div>
-			</div>
+			</div>--}}
+			<div class="col-md-2"></div>
 			<div class="col-md-8">
 				<div class="panel panel-default">
 				
