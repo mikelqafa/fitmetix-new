c3a27c908e944e876e590f96b60cff727fb82207
Ankur Agarwal <ankur@novatree.com>
2018-01-16 00:02:31 +0530

    menifest added

--- public/themes/default/layouts/default.blade.php
+++ public/themes/default/layouts/default.blade.php
@@ -6,18 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf_token" content="{!! csrf_token() !!}"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e7c82">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png') }}/">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{asset('manifest.json') }}">
+    <link rel="mask-icon" href="{{asset('safari-pinned-tab.svg') }}" color="#1e7c82">
     <meta name="theme-color" content="#1e7c82">
     <meta name="keywords" content="{{ Setting::get('meta_keywords') }}">
     <meta name="description" content="{{ Setting::get('meta_description') }}">
     <meta name="_token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{!! url('setting/'.Setting::get('favicon')) !!}">
+    <link rel="icon" type="image/x-icon" href="{!! asset('favicon.ico') !!}">
     <meta content="{{ url('/') }}" property="og:url"/>
     <meta content="{!! url('setting/'.Setting::get('logo')) !!}" property="og:image"/>
     <meta content="{{ Setting::get('meta_description') }}" property="og:description"/>
