4f2269cff14abe78d7865a9b7aa03402a7ca6a73
Ankur Agarwal <ankur@novatree.com>
2018-01-15 23:49:18 +0530

    menifest added

--- public/themes/default/layouts/default.blade.php
+++ public/themes/default/layouts/default.blade.php
@@ -8,22 +8,12 @@
     <meta name="csrf_token" content="{!! csrf_token() !!}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height"/>
-
-
-    <!-- Add to home screen for Android and modern mobile browsers -->
-    <link rel="manifest" href="manifest.json">
-    <meta name="theme-color" content="#1E7C82">
-
-    <!-- Add to home screen for Safari on iOS -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="white">
-    <meta name="apple-mobile-web-app-title" content="Fitmetix">
-    <link rel="apple-touch-icon" href="/setting/logo.png">
-    <!-- Add to home screen for Windows -->
-    <meta name="msapplication-TileImage" content="/setting/logo.png">
-    <meta name="msapplication-TileColor" content="#FFFFFF">
-
-
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e7c82">
+    <meta name="theme-color" content="#1e7c82">
     <meta name="keywords" content="{{ Setting::get('meta_keywords') }}">
     <meta name="description" content="{{ Setting::get('meta_description') }}">
     <meta name="_token" content="{{ csrf_token() }}">
