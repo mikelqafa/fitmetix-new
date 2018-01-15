8aec4eba3fb5997b86972f453c62eee86d374c37
Ankur Agarwal <ankur@novatree.com>
2018-01-16 00:15:47 +0530

    menifest added

--- public/themes/default/layouts/default.blade.php
+++ public/themes/default/layouts/default.blade.php
@@ -7,12 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf_token" content="{!! csrf_token() !!}"/>
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png') }}/">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png') }}">
-    <link rel="manifest" href="{{asset('manifest.json') }}">
-    <link rel="mask-icon" href="{{asset('safari-pinned-tab.svg') }}" color="#1e7c82">
-    <link rel="icon" type="image/x-icon" href="{!! asset('favicon.ico') !!}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e7c82">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <meta name="theme-color" content="#1e7c82">
     <meta name="keywords" content="{{ Setting::get('meta_keywords') }}">
     <meta name="description" content="{{ Setting::get('meta_description') }}">
--- public/themes/default/layouts/guest.blade.php
+++ public/themes/default/layouts/guest.blade.php
@@ -6,12 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf_token" content="{!! csrf_token() !!}"/>
         <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png') }}/">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/manifest.json">
-        <link rel="mask-icon" href="{{asset('safari-pinned-tab.svg') }}" color="#1e7c82">
-        <link rel="icon" type="image/x-icon" href="{!! asset('favicon.ico') !!}">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1e7c82">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
         <meta property="og:image" content="{{ url('setting/logo.jpg') }}" />
         <meta property="og:title" content="{{ Setting::get('site_title') }}" />
