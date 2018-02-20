d02dcce31e982e688e85cfefe14695457db8ac56
Ankur Agarwal <ankur@novatree.com>
2018-02-20 13:02:14 +0530

    minification

--- public/themes/default/assets/js/socket.js
+++ public/themes/default/assets/js/socket.js
@@ -1,16 +1,1 @@
-var server = require('http').Server();
-var io = require('socket.io')(server);
-var Redis = require('ioredis');
-
-
-var redis = new Redis();
-
-redis.subscribe('vijay-notification-created');
-
-redis.on('message',function(channel, message){
-    var message = JSON.parse(message)
-    console.log(channel,message)
-    io.emit(channel,message);
-});
-
-server.listen(3000);
+var server=require("http").Server(),io=require("socket.io")(server),Redis=require("ioredis"),redis=new Redis;redis.subscribe("vijay-notification-created"),redis.on("message",function(e,r){r=JSON.parse(r);console.log(e,r),io.emit(e,r)}),server.listen(3e3);
